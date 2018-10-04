7774b9a2b54cfc74ed7f38468ff26446040384f8
invisnet <classicpress@invis.net>
2018-10-04 19:49:47 +0000

    Bump version

--- switch-to-classicpress-from-wordpress.php
+++ switch-to-classicpress-from-wordpress.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Switch to ClassicPress from WordPress
  * Plugin URI:        https://github.com/classicpress/classicpress-migration-plugin
  * Description:       Switch to ClassicPress from WordPress.
- * Version:           1.0.0
+ * Version:           2.0.0
  * Tested up to:      4.9
  * Author:            ClassicPress
  * Author URI:        https://www.classicpress.net
@@ -23,29 +23,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Actions and Filters.
+ * Actions and Filters. 
+ *  
+ * @since 1.0.0 
  */
 add_filter( 'plugins_loaded', 'classicpress_load_plugin_textdomain' );
 add_filter( 'muplugins_loaded', 'classicpress_load_muplugin_textdomain' );
 add_filter( 'admin_init', 'classicpress_remove_gutenberg_dashboard_widget' );
 
 /**
- * Load Plugin Textdomain.
+ * Load Plugin Textdomain. 
+ *  
+ * @since 1.0.0 
  */
 function classicpress_load_plugin_textdomain() {
 	load_plugin_textdomain( 'switch-to-classicpress-from-wordpress', false, basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 /**
- * Load MU-Plugin (dir) Textdomain.
+ * Load MU-Plugin (dir) Textdomain. 
+ *  
+ * @since 1.0.0  
  */
 function classicpress_load_muplugin_textdomain() {
 	load_muplugin_textdomain( 'switch-to-classicpress-from-wordpress', basename( dirname( __FILE__ ) ) . '/languages' );
 }
 
 /**
- * Remove Gutenberg Dashboard Widget.
+ * Remove Gutenberg Dashboard Widget. 
+ *  
+ * @since 1.0.0  
  */
 function classicpress_remove_gutenberg_dashboard_widget() {
 	remove_filter( 'try_gutenberg_panel', 'wp_try_gutenberg_panel' );
 }
+
+
